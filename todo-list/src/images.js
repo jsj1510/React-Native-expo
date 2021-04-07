4793516157b68c132a958c38aad931b17c1d3b74
jsj <tw4504@naver.com>
2021-04-07 11:53:08 +0900

    todo-list ansyc storage

---
+++ todo-list/src/images.js
@@ -0,0 +1,12 @@
+import CheckBoxOutline from '../assets/icons/checkout.png'
+import CheckBox from '../assets/icons/checkbox.png'
+import DeleteForver from '../assets/icons/delete.png'
+import Edit from '../assets/icons/edit.png'
+
+export const images = {
+    uncompleted: CheckBoxOutline,
+    completed: CheckBox,
+    delete: DeleteForver,
+    update: Edit,
+};
+
