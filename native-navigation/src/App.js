c5da3bd7ce14876409859711b2373e42ceddf469
jsj <tw4504@naver.com>
2021-04-08 15:59:21 +0900

    탭바 아이콘, 활성회,비활성화

--- native-navigation/src/App.js
+++ native-navigation/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './navigations/Stack';
+import TabNavigation from './navigations/Tab';
 
 // const Container = styled.View`
 //     flex: 1;
@@ -12,7 +13,7 @@ import StackNavigation from './navigations/Stack';
 function App() {
     return (
         <NavigationContainer>
-            <StackNavigation />
+            <TabNavigation />
         </NavigationContainer>
     )
 }
