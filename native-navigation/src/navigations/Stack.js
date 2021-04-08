0fba48e641d0d8de74701cd364f39d2ab04a08e3
jsj <tw4504@naver.com>
2021-04-08 15:12:47 +0900

    헤더감추기, 헤더버튼

--- native-navigation/src/navigations/Stack.js
+++ native-navigation/src/navigations/Stack.js
@@ -3,6 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import List from '../screens/List';
 import Item from '../screens/Item';
+import { Platform } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const Stack = createStackNavigator();
 const StackNavigation = () => {
@@ -18,8 +21,28 @@ const StackNavigation = () => {
             <Stack.Screen 
                 name="List" 
                 component={List} 
-                options={{ headerTitle:"List screen"}}
-            />
+                options={{ 
+                    headerTitle:"List screen",
+                    headerBackTitleVisible: true,
+                    headerBackTitle: 'Prev',
+                    headerTitleStyle: { fontSize: 24 },
+                    headerTintColor: '#e74c3c',
+                    headerBackImage: ({ tintColor }) => {
+                        const style = {
+                          marginRight: 5,
+                          marginLeft: Platform.OS === 'ios' ? 11 : 0,
+                        };
+                        return (
+                          <MaterialCommunityIcons
+                            name="keyboard-backspace"
+                            size={30}
+                            color={tintColor}
+                            style={style}
+                          />
+                        );
+                    },
+                }}
+                />
             <Stack.Screen name="Detail" component={Item} />
         </Stack.Navigator>
        
--- native-navigation/src/screens/List.js
+++ native-navigation/src/screens/List.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'react-native';
 import styled from 'styled-components';
 
+
 const Container = styled.View`
     flex: 1;
     justify-content: center;
